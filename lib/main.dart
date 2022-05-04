584b9e827e893d259cc1f32fd937e998f41ab2b1
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-05 00:37:56 +0530

    Adding Layour widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
+//importing the base class file that flutter provides functionality
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); //run app
 }
 
 //inheriting the class
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal',
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My Quiz App'),
+          ),
+          body: Column(
+            //Column like format
+            children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
     );
   }
 }
